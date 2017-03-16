cc0866a85cb4169319b4a07561b9bf17eda48926
Andreas Hamacher <andreashamacher@gmail.com>
2017-03-16 16:00:36 +1100

    adding andreas debuggingdataset as comment

--- gigapoint/omegalib_module_test/gigapoint.py
+++ gigapoint/omegalib_module_test/gigapoint.py
@@ -1,4 +1,5 @@
 import gigapoint
 
 gp = gigapoint.initialize()
-gp.initPotree("gigapoint_resource/config/gigapoint_century_local.json")
+-gp.initPotree("gigapoint_resource/config/gigapoint_century_local.json")
+#gp.initPotree("gigapoint_resource/config/gigapoint_some_tree.json")
